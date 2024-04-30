e87295a35753678b7933ee7354c62f0f604bb6e2
Yana <yanok87@users.noreply.github.com>
2024-04-30 18:49:48 +0200

    add recursion and command line arguments

--- task_1/main.py
+++ task_1/main.py
@@ -2,40 +2,60 @@
 
 from pathlib import Path
 import shutil
-import os
 
 
-def copy_files(source_dir: Path, destination_dir: str = "dist") -> None:
+def parse_input(user_input):
+    """This function parses user input"""
+    command, *args = user_input.split()
+    command = command.strip().lower()
+    return command, *args
+
+
+def copy_files(source_dir: Path, destination_dir: str) -> None:
     """This function creates a new folder and copies the contens of the pictures folder"""
+    source_dir = Path(source_dir)
+    destination_dir = Path(destination_dir)
 
-    source_dir = os.path.abspath(source_dir)
-    destination_dir = os.path.abspath(destination_dir)
+    if source_dir.is_dir():
+        for child in source_dir.iterdir():
+            if child.is_file():
+                extension = child.suffix[1:]
+                destination_subdir = destination_dir / extension
+                destination_subdir.mkdir(parents=True, exist_ok=True)
+                destination_file = destination_subdir / child.name
+                print("destination_file ", destination_file)
 
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
+                try:
+                    shutil.copyfile(child, destination_file)
+                except Exception as e:
+                    print(f"Error copying file {child}: {e}")
 
-    for root, dirs, files in os.walk(source_dir):
-        for file in files:
-            source_path = os.path.join(root, file)
-            extension = os.path.splitext(file)[1][1:]
-            destination_subdir = os.path.join(destination_dir, extension)
+            else:
+                copy_files(child, destination_dir)
 
-            if not os.path.exists(destination_subdir):
-                os.makedirs(destination_subdir)
 
-            destination_file = os.path.join(destination_subdir, file)
+def main():
+    """This function prompts user to enter a path to folder and a destination folder"""
 
+    while True:
+        user_input = input("Enter a path to folder to copy and a destination folder: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        else:
+            folder = command
+            destination = args[0] if len(args) > 0 else "dist"
             try:
-                shutil.copyfile(source_path, destination_file)
+                copy_files(folder, destination)
+                print("Files copied successfully.")
+                break
             except Exception as e:
-                print(f"Error copying file {source_path}: {e}")
+                print(f"An error occurred: {e}")
+                break
 
 
 if __name__ == "__main__":
 
-    folder = Path("/Users/ianamatrosova/dev/GoIT/algos/goit-algo-hw-03/task_1/pictures")
-    try:
-        copy_files(folder)
-        print("Files copied successfully.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    main()
